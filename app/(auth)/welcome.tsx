824e3eea693fca5da0d8dc7611bcc1637e34d3e3
shaheenshay <shaheen.shay@gmail.com>
2024-08-28 15:15:23 +0530

    welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,17 @@
+import { router } from "expo-router";
 import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const OnBoarding = () => {
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
-      <TouchableOpacity>
-        <Text>Skip</Text>
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
