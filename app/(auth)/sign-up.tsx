9c891cc32c9d95f6dd73caab704e6b4bccbcb9e1
shaheenshay <shaheen.shay@gmail.com>
2024-08-29 16:09:42 +0530

    sign-up page clerk/expo init

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,18 +2,72 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
 import { useState } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
 
-  const onSignUpPress = async() => {};
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) return;
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerification({
+        ...verification,
+        state: "pending",
+      });
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      if (completeSignUp.status === "complete") {
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerification({ ...verification, state: "success" });
+      } else {
+        setVerification({
+          ...verification,
+          error: "Verification failed.",
+          state: "failed",
+        });
+      }
+    } catch (err: any) {
+      setVerification({
+        ...verification,
+        error: err.errors[0].longMessage,
+        state: "failed",
+      });
+    }
+  };
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -30,7 +84,7 @@ const SignUp = () => {
             placeholder="Enter name"
             icon={icons.person}
             value={form.name}
-            />
+          />
           <InputField
             label="Email"
             placeholder="Enter email"
@@ -49,13 +103,18 @@ const SignUp = () => {
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
 
-          <CustomButton title="Sign Up" onPress={onSignUpPress}
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
             className="mt-6"
           />
 
           <OAuth />
 
-          <Link href="/sign-in" className="text-sm text-center text-general-200 mt-10">
+          <Link
+            href="/sign-in"
+            className="text-sm text-center text-general-200 mt-10"
+          >
             <Text>Already have an account? </Text>
             <Text className="text-primary-500"> Log In</Text>
           </Link>
