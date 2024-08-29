96e22a8568cbf182416390e6105afe7f151daac2
shaheenshay <shaheen.shay@gmail.com>
2024-08-29 14:27:45 +0530

    sign-up page inputfield added

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,15 +1,55 @@
-import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import { useState } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async() => {};
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-              Create Your Account
+            Create your Account
           </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton title="Sign Up" onPress={onSignUpPress}
+            className="mt-6"
+          />
         </View>
       </View>
     </ScrollView>
